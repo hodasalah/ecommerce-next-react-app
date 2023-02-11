e16e2cea43fdb0ddefe7622070ee5a1fe48ba7a0
hoda salah <hodasalah35@gmail.com>
2023-02-11 22:04:38 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+/** first get path object from node */ 
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  /** second add sass config */ 
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import './base.scss';`
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+// first step  after finish store configration
+import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import store from "../store"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// second step is create persistor
+let persistor = persistStore(store)
+
+export default function App({ Component, pageProps })
+{
+  //third step is to wrap our app in provider and persistGate
+  return <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Component {...pageProps} />
+    </PersistGate>
+  </Provider>
 }
+//forth step close and create cartSlice in store folder
