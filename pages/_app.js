60d9e89a4d7d440abceb43813e8f2361d9bd8e34
hoda salah <hodasalah35@gmail.com>
2023-02-19 00:39:18 +0300

    setup nextauth with mongodb

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.scss'
 // first step  after finish store configration
+import { SessionProvider } from "next-auth/react"
 import Head from 'next/head'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
@@ -10,7 +11,7 @@ import store from "../store"
 // second step is create persistor
 let persistor = persistStore(store)
 
-export default function App({ Component, pageProps })
+export default function App({ Component, pageProps: { session, ...pageProps } })
 {
   //third step is to wrap our app in provider and persistGate
   return (
@@ -20,13 +21,15 @@ export default function App({ Component, pageProps })
         <meta name="description" content="HODA_SHOP is an inline shopping app for all" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={favicon.src} sizes="512x512" />
-        
+
       </Head>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </SessionProvider>
     </>
   )
 }
