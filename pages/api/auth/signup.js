a4822a4ab37aa11bcd402677a6443ed493066ed5
hoda salah <hodasalah35@gmail.com>
2023-03-07 21:39:20 +0300

    create db shema[user]

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,14 +1,15 @@
+import User from "@/models/User";
+import { createActivationToken } from '@/utils/tokens';
 import { validateEmail } from "@/utils/validation";
+import bcrypt from "bcrypt";
 import nc from "next-connect";
 import db from "../../../utils/db";
-
 const handler = nc()
 handler.post(async (req, res) =>
 {
   try {
     // connect out database
     await db.connectDb()
-    console.log(req.body)
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
       return res.status(400).json({ message: "Please enter your name and email and your password." });
@@ -16,7 +17,21 @@ handler.post(async (req, res) =>
     if (!validateEmail(email)) {
       return res.status(400).json({ message: "Invalid Email." })
     }
-    console.log(validateEmail(email))
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "This email is already exists." })
+    }
+    if (password.length < 8) {
+      return res.status(400).json({ message: "Password must be at least 8 characters." })
+    }
+    const cryptedPassword = await bcrypt.hash('password', 12);
+    const newUser = new User({ name, email, password: cryptedPassword })
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    })
+    res.send(activation_token);
+
     return res.status(200).json({ message: "Sign up successfully" })
   } catch (error) {
     res.status(500).json({ message: error.message })
